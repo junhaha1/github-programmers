3c15bc30d8db8bdf6c81b25951845e19aaf541cc
Kim Jun Ha <junhaha3@gamil.com>
2021-07-03 21:53:48 +0900

    Change keypad

--- keypad.py
+++ keypad.py
@@ -1,14 +1,17 @@
 def solution(numbers, hand):
     answer = ''
-    keyboard = [[1,2,3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]
+    keyboard = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]
     
     left = [0, 3]
     right = [2, 3]
 
     for i in numbers:
-        print(i)
+        if i in [1, 4, 7, '*']:
+            answer += 'L'
+        elif i in  [3, 6, 9, '#']:
+            answer += 'R'
     
     return answer
 
 
-print(solution([1,2,3,4], "right"))
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
