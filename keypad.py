a1d7522cde1b310dd8774b92f1e382c2c4717496
Kim Jun Ha <junhaha3@gamil.com>
2021-07-03 22:12:18 +0900

    Complete PROB

--- keypad.py
+++ keypad.py
@@ -5,11 +5,19 @@ def solution(numbers, hand):
     left = [0, 3]
     right = [2, 3]
 
+    #좌표상 거리 계산으로 접근
     for i in numbers:
+        #좌표값 계산
+        temp_position = [(x, y) for x in range(len(keyboard)) for y in range(len(keyboard[0])) if keyboard[x][y] == i]
+
         if i in [1, 4, 7, '*']:
+            left = list(temp_position)
             answer += 'L'
         elif i in  [3, 6, 9, '#']:
+            right = list(temp_position)
             answer += 'R'
+        elif i in  [2, 5, 8, 0]:
+            print(temp_position)
     
     return answer
 
