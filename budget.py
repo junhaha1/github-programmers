394515ddf2a0666b65a56ef8613bd534b5880452
Kim Jun Ha <junhaha3@gamil.com>
2021-07-05 17:17:46 +0900

    SUCCESS budget

--- budget.py
+++ budget.py
@@ -1,7 +1,13 @@
 def solution(d, budget):
     answer = 0
     if sum(d) > budget:
-        pass
+        temp = 0
+
+        for i in sorted(d):
+            temp += i
+            if temp > budget:
+                break
+            answer += 1
             
 
     elif sum(d) <= budget:
@@ -9,4 +15,5 @@ def solution(d, budget):
     
     return answer
 
-print(solution([2,2,3,3], 10))
+#itertools 에서 combinations를 이용하면 시간복잡도가 심각해짐. 
+#그래서 조합을 이용하지 않고 풀기
