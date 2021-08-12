897f47e7c0a200128880173d887fa55db18ec289
Kim Jun Ha <junhaha3@gamil.com>
2021-08-12 21:15:50 +0900

    SUCCESS

--- student_score.py
+++ student_score.py
@@ -31,5 +31,3 @@ def solution(scores):
         answer += char
             
     return answer
-
-print(solution([[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]]))
