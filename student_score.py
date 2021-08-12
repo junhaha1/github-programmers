8a54a4850d66f47a7534dc8024776681ce853061
Kim Jun Ha <junhaha3@gamil.com>
2021-08-12 21:14:33 +0900

    SUCCESS

--- student_score.py
+++ student_score.py
@@ -1,39 +1,35 @@
 def solution(scores):
     temp = []
-    count = 0
     answer = ''
 
     #각자 자기 자신의 점수 모음
-    for i in scores: 
-        temp.append(i[count])
-        count += 1
-
-    count = 0
+    for i in range(len(scores)): 
+        temp.append(scores[i][i])
     
-    #평균 구하기
-    for i in scores:
-        if i[count] == max(temp):
-            i.remove(max(temp))
-        elif i[count] == min(temp):
-            i.remove(min(temp))
-        count += 1
-
-        print(i)
-
-        avg = sum(i) / len(i)
+    for i in range(len(scores)):
+        temp2 = 0
+        count = 0
         char = ''
-        if avg >= 90:
-            char = 'A'
-        elif avg >= 80 and avg < 90:
-            char = 'B'
-        elif avg >= 70 and avg < 80:
-            char = 'C'
-        elif avg >= 50 and avg < 70:
-            char = 'D'
-        else:
-            char = 'F'
-        answer += char
+        for j in range(len(scores)):
+            if j == i and (scores[j][i] == max(temp) or scores[j][i] == min(temp)):
+                continue
+            temp2 += scores[j][i]
+            count += 1
+        avg = temp2 / count
 
+        if avg >= 90:
+            char = "A"
+        elif 80<= avg < 90:
+            char = "B"
+        elif 70<= avg < 80:
+            char = "C"
+        elif 50<= avg < 70:
+            char = "D"
+        else:
+            char = "F"
+        
+        answer += char
+            
     return answer
 
-print(solution([[50,90],[50,87]]))
+print(solution([[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]]))
