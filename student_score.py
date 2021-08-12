953f5d44a038c53c9197daf6e47017543b209711
Kim Jun Ha <junhaha3@gamil.com>
2021-08-12 21:36:09 +0900

    SUCCESS

--- student_score.py
+++ student_score.py
@@ -1,21 +1,22 @@
 def solution(scores):
     temp = []
     answer = ''
+    count = 0
 
     #각자 자기 자신의 점수 모음
     for i in range(len(scores)): 
-        temp.append(scores[i][i])
+        temp2 = []
+        for j in  range(len(scores)):
+            temp2.append(scores[j][i])
+        temp.append(temp2)
     
-    for i in range(len(scores)):
-        temp2 = 0
-        count = 0
-        char = ''
-        for j in range(len(scores)):
-            if j == i and (scores[j][i] == max(temp) or scores[j][i] == min(temp)):
-                continue
-            temp2 += scores[j][i]
-            count += 1
-        avg = temp2 / count
+    for i in range(len(temp)):
+        if temp[i].count(max(temp[i])) == 1 and scores[i][i] == max(temp[i]):
+            temp[i].remove(max(temp[i]))
+        elif temp[i].count(min(temp[i])) == 1 and scores[i][i] == min(temp[i]):
+            temp[i].remove(min(temp[i]))
+        
+        avg = sum(temp[i]) / len(temp[i])
 
         if avg >= 90:
             char = "A"
