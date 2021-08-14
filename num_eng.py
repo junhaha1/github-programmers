1585e420e19fcfeaa019c6eb55824c2e01a4f070
Kim Jun Ha <junhaha3@gamil.com>
2021-08-14 21:41:17 +0900

    SUCCESS

--- num_eng.py
+++ num_eng.py
@@ -1,3 +1,5 @@
 def solution(s):
-    answer = 0
-    return answer
+    number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for i, j in enumerate(number):
+        s = s.replace(j, str(i))
+    return s
