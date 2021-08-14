63976c901d2c0437714d109cf5b7046eb4797971
Kim Jun Ha <junhaha3@gamil.com>
2021-08-14 21:42:56 +0900

    SUCCESS

--- num_eng.py
+++ num_eng.py
@@ -2,4 +2,4 @@ def solution(s):
     number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     for i, j in enumerate(number):
         s = s.replace(j, str(i))
-    return s
+    return int(s)
