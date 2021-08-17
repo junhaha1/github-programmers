79df58d0ed40f9221b838f59f10ffcae63086243
Kim Jun Ha <junhaha3@gamil.com>
2021-08-17 18:42:41 +0900

    SUCCESS Three_deci

---
+++ Level1/Three_deci.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = []
+    result = 0
+    j = 0
+
+    #3진법 전환하여 뒤집어서 리스트에 넣음
+    while n > 0:
+        answer.append(n % 3)
+        n = n // 3
+    
+    for i in reversed(answer):
+        result += i * (3 ** j)
+        j += 1
+        
+    return result
+
+print(solution(45))
